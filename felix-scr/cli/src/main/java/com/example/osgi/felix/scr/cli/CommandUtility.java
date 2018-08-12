a57cfd78ed406b9f579d6cc50d920f99e0e1d660
Resul Avan <Resul.Avan@argela.com.tr>
2018-08-12 22:06:28 +0300

    update packages

---
+++ felix-scr/cli/src/main/java/com/example/osgi/felix/scr/cli/CommandUtility.java
@@ -0,0 +1,19 @@
+package com.example.osgi.felix.scr.cli;
+
+import org.slf4j.Logger;
+
+/**
+ * Created by resulav on 11.08.2018.
+ */
+public class CommandUtility {
+
+	static final String SCOPE = "osgi-demo";
+
+	public static void calledCommand(Logger logger, String command) {
+		logger.debug("{}:{} is called", SCOPE, command);
+	}
+
+	public static void finishedCommand(Logger logger, String command) {
+		logger.debug("{}:{} is finished", SCOPE, command);
+	}
+}
---
+++ felix-scr/cli/src/main/java/com/example/osgi/felix/scr/cli/SampleCommand.java
@@ -0,0 +1,37 @@
+package com.example.osgi.felix.scr.cli;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.slf4j.Logger;
+
+import com.example.osgi.api.SampleService;
+
+/**
+ * Created by resulav on 11.08.2018.
+ */
+@Command(scope = CommandUtility.SCOPE, name = SampleCommand.COMMAND, description = "Returns sample service name")
+@Service
+public class SampleCommand implements Action {
+
+	private final Logger logger = getLogger(getClass());
+
+	static final String COMMAND = "sample";
+
+	@Reference
+	private SampleService service;
+
+	@Override
+	public Object execute() throws Exception {
+		try {
+            CommandUtility.calledCommand(logger, COMMAND);
+            return service.getName();
+        }finally {
+            CommandUtility.finishedCommand(logger, COMMAND);
+        }
+	}
+}
